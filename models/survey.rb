c5f62ba98a32d6278072c292ea527ce2b878a79c
bbeaird <charles.amoako.128@gmail.com>
2013-10-22 15:37:12 -0700

    wrote model shoulda tests for database

--- models/survey.rb
+++ models/survey.rb
@@ -1,4 +1,6 @@
 class Survey < ActiveRecord::Base
+  validates :survey_url, :survey_info, presence: true
+
   belongs_to :user
   has_many :survey_movies
   has_many :movies, through: :survey_movies
