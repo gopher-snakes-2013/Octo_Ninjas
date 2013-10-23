6d6db536c6d70ddf70db603199ce0bab8c214bdc
bbeaird <charles.amoako.128@gmail.com>
2013-10-23 12:14:08 -0700

    refactors

--- app.rb
+++ app.rb
@@ -1,18 +1,8 @@
-require 'sinatra'
-require 'sinatra/activerecord'
-require 'bcrypt'
-require 'securerandom'
-require 'rottentomatoes'
-
-require './models/movie'
-require './models/survey'
-require './models/survey_movie'
-require './models/user'
-require './models/vote'
+%w(sinatra sinatra/activerecord bcrypt securerandom rottentomatoes ./models/movie ./models/survey ./models/survey_movie ./models/user ./models/vote).each {|dependency| require dependency}
 
 include RottenTomatoes
 
-Rotten.api_key = 'z9rcfwsnkafcdrwpxhqsaqqy'
+Rotten.api_key = 'z9rcfwsnkafcdrwpxhqsaqqy' ## Thanks for your key bro
 
 begin
   require 'dotenv'
@@ -25,24 +15,39 @@ enable :sessions
 set :database, ENV['DATABASE_URL'] || "postgres://localhost/ninjadb"
 
 def current_user
-  @current_user ||= logged_in? && User.find(session[:user_id])
+  @current_user ||= User.find(session[:user_id]) 
+  #What is the the point of loggedin if current user takes the value of User.find?
 end
 
 def logged_in?
   session[:user_id]
+  #Although this is a truthy value its not a boolean
 end
 
 def enforce_login
-  redirect to '/' if !logged_in?
+  redirect '/' unless logged_in? ## unless rather than !if
 end
 
 def check_existing_user(username)
-  if user = User.find_by_username(username)
+  user = User.find_by_username(username)
+  if user  
     return user
   else
     return false
   end
+end
 
+def add_to_movie_list
+  @my_movie_list = []
+  @my_movie = RottenMovie.find(:title => my_title, :limit => 1)
+
+  @my_movie_list << Movie.create(:title => @my_movie.title,
+                                 :synopsis => @my_movie.synopsis, 
+                                 :runtime => @my_movie.runtime, 
+                                 :critics_score => @my_movie.ratings.critics_score, 
+                                 :audience_score => @my_movie.ratings.audience_score, 
+                                 :pic => @my_movie.posters.original
+  )
 end
 
 get '/' do
@@ -87,16 +92,7 @@ end
 post '/create_survey' do
   @user = current_user
 	my_title = params[:movie_title]
-	@my_movie_list = []
-	@my_movie = RottenMovie.find(:title => my_title, :limit => 1)
-
-	@my_movie_list << Movie.create(:title => @my_movie.title,
-							 									 :synopsis => @my_movie.synopsis, 
-							 									 :runtime => @my_movie.runtime, 
-							 									 :critics_score => @my_movie.ratings.critics_score, 
-							 									 :audience_score => @my_movie.ratings.audience_score, 
-							 									 :pic => @my_movie.posters.original
-	)
+  add_to_movie_list
 	erb :create_survey
 end
 
