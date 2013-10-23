395e0e9257988c49b9e06db3b50ea1091b538c4a
bbeaird <charles.amoako.128@gmail.com>
2013-10-22 18:57:12 -0700

    Implemented user registration and login with Bcrypt passwords Can create users in database Updated views to display login information and errors on wrong login enforce login on create_survey view use sessions to keep track of user across routes Renamed signup.erb to register.erb created logout route

--- app.rb
+++ app.rb
@@ -17,37 +17,75 @@ end
 
 enable :sessions
 
-set :database, ENV['DATABASE_URL']
+set :database, ENV['DATABASE_URL'] || "postgres://localhost/ninjadb"
 
+def current_user
+  @current_user ||= logged_in? && User.find(session[:user_id])
+end
 
+def logged_in?
+  session[:user_id]
+end
+
+def enforce_login
+  redirect to '/' if !logged_in?
+end
+
+def check_existing_user(username)
+  if user = User.find_by_username(username)
+    return user
+  else
+    return false
+  end
+
+end
 
 get '/' do
+  @user = current_user if logged_in?
   erb :index
 end
 
 post '/signin' do
-	
+  @user = User.authenticate(params[:username], params[:password])
+  if !@user.nil?
+    session[:user_id] = @user.id
+    redirect '/create_survey'
+  else
+    @error = "Wrong username or password"
+    erb :index
+  end
 end
 
 get '/register' do
-	erb :signup
+	erb :register
 end
 
 post '/register' do
-	redirect '/create_survey'
+  if !check_existing_user(params[:username])
+    @user = User.create( {username: params[:username], email: params[:email], password: params[:password]} )
+    session[:user_id] = @user.id
+  	redirect '/create_survey'
+  else
+    @error = "That username already exists"
+    erb :index
+  end
 end
 
 get '/create_survey' do
+  enforce_login
+  @user = current_user
   erb :create_survey
 end
 
-post '/create_survey do'
+post '/create_survey' do
 
 
 end
 
-
-
+post '/logout' do
+  session.clear
+  redirect '/'
+end
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -37,8 +37,9 @@ ActiveRecord::Schema.define(version: 20131022024439) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "name"
+    t.string   "username"
     t.string   "email"
+    t.string   "password_hash"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
